458ef5d05d848b12526e9fd2cfe7e6f7e6b59c66
loChris <44450518+loChris@users.noreply.github.com>
2020-05-08 16:21:19 -0400

    player can now jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 8f;
+    [SerializeField] private float jumpForce = 10f;
     [SerializeField] private Rigidbody2D playerRigidBody;
     
     // Start is called before the first frame update
@@ -18,5 +19,10 @@ public class Player : MonoBehaviour
     { 
         float horizontalMove = moveSpeed * Input.GetAxis("Horizontal");
         playerRigidBody.velocity = new Vector2(horizontalMove, playerRigidBody.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, jumpForce);
+        }
     }
 }
